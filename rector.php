7d3f0ef27167d78b91721b23a121ea324693236b
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-10-29 09:30:56 +0100

    update

--- rector.php
+++ rector.php
@@ -4,12 +4,27 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\SetList;
-use RectorLaravel\Rector\MethodCall\ValidationRuleArrayStringValueToArrayRector;
 use RectorLaravel\Set\LaravelSetList;
+use RectorLaravel\Set\LaravelSetProvider;
 
 return RectorConfig::configure()
-    ->withRules([
-        ValidationRuleArrayStringValueToArrayRector::class,
+    ->withSetProviders(LaravelSetProvider::class)
+    ->withSets([
+        LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL,
+        LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_COLLECTION,
+        LaravelSetList::LARAVEL_CONTAINER_STRING_TO_FULLY_QUALIFIED_NAME,
+        LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER,
+        LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
+        LaravelSetList::LARAVEL_FACTORIES,
+        LaravelSetList::LARAVEL_IF_HELPERS,
+        LaravelSetList::LARAVEL_LEGACY_FACTORIES_TO_CLASSES,
+        SetList::DEAD_CODE,
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::PHP_83,
+        SetList::TYPE_DECLARATION,
     ])
     ->withPaths([
         __DIR__.'/app',
@@ -19,17 +34,14 @@ return RectorConfig::configure()
         __DIR__.'/resources',
         __DIR__.'/routes',
     ])
-
-    ->withImportNames(true)
-    // here we can define, what prepared sets of rules will be applied
-    ->withSets([
-        SetList::DEAD_CODE,
-        SetList::CODE_QUALITY,
-        SetList::CODING_STYLE,
-        SetList::PHP_83,
-        SetList::TYPE_DECLARATION,
-        LaravelSetList::LARAVEL_CODE_QUALITY,
-        LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
-        LaravelSetList::LARAVEL_COLLECTION,
-        LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL,
-    ]);
+    ->withImportNames()
+    ->withParallel()
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        strictBooleans: true,
+    )
+    ->withPhpSets();
